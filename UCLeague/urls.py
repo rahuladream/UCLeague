dd5613cb679dce85b471b36dc24ada2d65779756
Rahul Singh <rahul.singh4800@gmail.com>
2020-09-09 23:25:57 +0530

    yet another swagger added

--- UCLeague/urls.py
+++ UCLeague/urls.py
@@ -19,9 +19,31 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
 
+# Yet another swagger import 
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
 # Local Import
+from .constants import *
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title=DOCS_TITLE,
+      default_version=DOCS_VERSION,
+      description=DOCS_DESCRIPTION,
+      terms_of_service=DOCS_TERM_SERVICE,
+      contact=openapi.Contact(email=DOCS_CONTACT_HEAD),
+      license=openapi.License(name=DOCS_LICENSE),
+   ),
+   public=DOCS_VISIBLE_PUBLIC
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^api/', include('app.club.urls', namespace='club_fetch'))
+    url(r'^api/', include('app.club.urls', namespace='club_fetch')),
+
+    url(r'^docs(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='docs-json'),
+    url(r'^$', schema_view.with_ui('swagger', cache_timeout=0), name='docs-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
 ]
